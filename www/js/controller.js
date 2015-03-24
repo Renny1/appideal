41f7be65d458bc2f7d5aa035d6dfc35e9ea5d942
Renan de Aguiar Oliveira <renan.aguiar.o@gmail.com>
2015-03-24 14:58:07 -0300

    Páginas services controllers e app's montados para uso

---
+++ www/js/controller.js
@@ -0,0 +1,34 @@
+angular.module('starter.controllers',['ionic'])
+
+.controller('LoginCtrl', function($scope, LoginService, PostService, $ionicPopup, $state, $http, IDEALFactory, $ionicLoading) {
+    $scope.data = {};
+
+    $scope.data.persistente = false;
+    $scope.login = function() {
+        $ionicLoading.show({template: 'Carregando...'});
+        LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+            IDEALFactory.setUser({
+                'username' : $scope.data.username
+            },true);
+            $ionicLoading.hide();
+            $state.go('home');
+        }).error(function(data) {
+            $ionicLoading.hide();
+            var alertPopup = $ionicPopup.alert({
+                title: 'Falha no login!',
+                template: 'Por favor, cheque sua conexão!'
+            });
+        });
+
+    }
+
+    $scope.forget = function(){
+     /* window.open('http://conexaojbs.agencianuts.com.br/i-o/lostpassword/','_blank', 'location=no');*/
+ };
+
+})
+
+.controller('MainCtrl', function($scope, $state, $ionicLoading, IDEALFactory, $rootScope) {
+
+
+});
